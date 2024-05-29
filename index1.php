204e9ff2f4526cfae93d03b4def6e25bb1f5c8cc
zuri-M <153556685+zuri-M@users.noreply.github.com>
2024-05-29 17:59:17 -0600

    Update index1.php

--- index1.php
+++ index1.php
@@ -5,6 +5,7 @@
     <title>IntroPhp7 Grupo XA</title>
     <link rel="stylesheet" href="css/menu.css"/>
     <link rel="stylesheet" href="css/stem.css"/>
+    <link rel="stylesheet" href="css/problemas.css"/>
     <link href="https://fonts.googleapis.com/css?family=Rambla" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Comfortaa" rel="stylesheet">
 </head>
